8b39375ddcd986571bae784fb64cc6cdc52d0999
Adam Curran <amdam7@gmail.com>
2023-01-21 10:51:32 -0700

    Sync from blade 1/21/23

---
+++ WTF/Account/310647873#1/Turalyon/Aureys/SavedVariables/Plater.lua
@@ -0,0 +1,17 @@
+
+PlaterDBChr = {
+	["spellRangeCheckRangeFriendly"] = {
+	},
+	["buffsBanned"] = {
+	},
+	["first_run3"] = {
+		["Player-3685-0E0731D6"] = true,
+	},
+	["resources_on_target"] = false,
+	["minimap"] = {
+	},
+	["debuffsBanned"] = {
+	},
+	["spellRangeCheckRangeEnemy"] = {
+	},
+}
